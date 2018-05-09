71e406b4b61c815fc69bb43826cae9e0fa8d98b6
Alec <acaputo@mail.smcvt.edu>
2018-05-08 23:08:08 -0400

    Graph changes and adding ability to customize times
    todo, break this into an exportable module

--- js/eth-detail.js
+++ js/eth-detail.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-	getDayOfData();
-	function getDayOfData(){
-		let today = moment().startOf('day').format()
+	
+	$('#timeFrame').on('change', function(){
+		let timeFrame = $(this).val();
+		console.log(timeFrame);
+		getData(timeFrame);
+	});
+
+	function getData(time){
+		//todo period == interval on graph, start time is what i meant the selector to be. swithc and change values based on that
+		let today = moment().startOf('day').format();
 		var promise = $.ajax({
-			url: "https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_ETH_USD/history?period_id=1HRS&time_start="+today,
+			url: "https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_ETH_USD/history?period_id="+time+"&time_start="+today,
 			headers: {
 				"X-CoinAPI-Key":"CB7FF527-26F2-4765-90FF-E04B852B1B01"
 			},
@@ -11,6 +18,7 @@ $(document).ready(function(){
 		});
 
 		promise.done(function(data){
+			$('#graph-load-spinner').hide();
 			let closePrices = [];
 			let times = [];
 			$.each(data,function(){
@@ -30,7 +38,7 @@ $(document).ready(function(){
 	}
 
 	function createChart(coinData,times){
-	var ctx = document.getElementById("myChart").getContext('2d');
+	var ctx = document.getElementById("eth-chart").getContext('2d');
 	var myChart = new Chart(ctx, {
 	    type: 'line',
 	    data: {
@@ -39,7 +47,7 @@ $(document).ready(function(){
 	    		label: "ETH Price Today",
 	    		data: coinData,
 	    		backgroundColor: "rgb(72, 115, 140)",
-	    		
+
 	    	}],
 	    },
 	    
--- views/details/eth_detail.php
+++ views/details/eth_detail.php
@@ -1,14 +1,26 @@
 <?php include('header.php'); ?>
 <!DOCTYPE html>
 <html>
-<div class="row">
-	<div class="col-xs-12 col-lg-9">
-		<div class="graph-window">
-			<canvas id="myChart" width="450" height="225"></canvas>
+	<div class="row">
+		<div class="col-xs-12 col-lg-9">
+			<div class="graph-window">
+				<canvas id="eth-chart" width="450" height="225"></canvas>
+				<i id="graph-load-spinner" class="fas fa-circle-notch fa-spin fa-5x"></i>
+			</div>
 		</div>
-	</div>
-	<div class="col-xs-12 col-lg-3">
-		<h1>Info</h1>
+		<div class="col-xs-12 col-lg-3">
+			<h1>Info</h1>
+			<div class="form-group">
+				<label for="timeFrame">Select A Time</label>
+			    <select class="form-control" id="timeFrame">
+			      <option value="1HRS">1 Hour</option>
+			      <option value="12HRS">12 Hours</option>
+			      <option value="1DAY">1 Day</option>
+			      <option values="7DAY">7 Days</option>
+			      <option value="1MNTH">1 Month</option>
+	    		</select>
+			</div>
+		</div>
 	</div>
 </html>
 <script src="../../js/eth-detail.js"></script>
